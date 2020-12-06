967aab0785dd7fc36ed5099acdac5b6b9cf4a5c1
Deny Tsjapanov <d.tsjapanov@gmail.com>
2020-12-07 00:59:57 +0100

    Added more styling, comments and press Enter for search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-//API datat from Edamam.com
+// API datat from Edamam.com
 const APP_ID = "9ab815fd";
 const API_KEY = "13b042fc8421195fa7db8ae62c6d0091";
 const baseUrl = `https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}`;
 var previouslySearchedData = {};
 
 
-// Event listener for button press
+// Event listener for button press on mouse click
 $(document).ready(function () {
     document.querySelector("#btn-id").addEventListener("click", function (event) {
         console.log("button pressed");
@@ -13,7 +13,17 @@ $(document).ready(function () {
     });
 });
 
+// Event listener for button press on "Enter" press
+var input = document.getElementById("search");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("btn-id").click();
+    }
+});
 
+
+// API request
 async function sendApiRequest() {
     var query = $("#search").val();
 
@@ -27,7 +37,7 @@ async function sendApiRequest() {
         dataType: 'json',
         success: function (data, textStatus, jqXHR) {
             updateHTML(data);
-            console.log(data)
+            console.log(data);
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -52,7 +62,7 @@ function updateHTML(data) {
             <img src="${hit.recipe.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${hit.recipe.label}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${hit.recipe.healthLabels[length]}, ${hit.recipe.calories} kCal.</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">${hit.recipe.healthLabels[length]}<br>${hit.recipe.calories} kCal.</h6>
                     <p class="card-text">${hit.recipe.source}</p>
                     <a href="${hit.recipe.url}" class="btn btn-default" target="_blank">Let's go!</a>
                 </div>
@@ -63,16 +73,3 @@ function updateHTML(data) {
 
     document.querySelector("#recipe-results").innerHTML = resultsHTML;
 }
-
-// Initialize AOS (www.https://michalsnik.github.io/aos/)
-function aos_init() {
-    AOS.init({
-        duration: 400,
-        easing: "ease-in-out",
-        once: false,
-        mirror: false
-    });
-}
-$(window).on('load', function () {
-    aos_init();
-});
