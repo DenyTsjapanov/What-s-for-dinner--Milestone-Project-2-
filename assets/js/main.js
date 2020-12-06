6172a800b92eea21258ff52c7228f9b3777fe3f5
Deny Tsjapanov <d.tsjapanov@gmail.com>
2020-12-06 01:28:59 +0100

    added more styling, more info on cards & AOS

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@ var previouslySearchedData = {};
 
 
 // Event listener for button press
-$(document).ready(function(){
-    document.querySelector("#btn-id").addEventListener("click", function(event) {
+$(document).ready(function () {
+    document.querySelector("#btn-id").addEventListener("click", function (event) {
         console.log("button pressed");
         sendApiRequest();
     });
@@ -16,24 +16,21 @@ $(document).ready(function(){
 
 async function sendApiRequest() {
     var query = $("#search").val();
-    
-    // Check if data is in previouslySearchedData
-    // else
-    
+
     console.log(query);
 
     let url = baseUrl + `&q=${query}`;
 
     $.ajax({
         url: url,
-	    method: "GET",
+        method: "GET",
         dataType: 'json',
-        success: function(data, textStatus, jqXHR) {
+        success: function (data, textStatus, jqXHR) {
             updateHTML(data);
-            // Fetch the results from Google Places --> Callback
-            // In that Callback, update the Map and the Markers
+            console.log(data)
+
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.log(errorThrown);
             alert("Couldn't fetch any data");
         },
@@ -47,21 +44,35 @@ function updateHTML(data) {
     let recipeResultsElement = document.querySelector("#recipe-results");
     recipeResultsElement.innerHTML = '';
     let resultsHTML = '';
-    
-    data.hits.forEach(function(hit){
+
+    data.hits.forEach(function (hit) {
         resultsHTML = resultsHTML + `
-    <div class="col-6 col-lg-3 col-md-4 mb-3 pt-3">
+    <div class="col-6 col-lg-3 col-md-4 mb-3 pt-3" data-aos="fade-up">
         <div class="card">
             <img src="${hit.recipe.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${hit.recipe.label}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${hit.recipe.dietLabels[0]}, ${hit.recipe.calories} calories.</h6>
                     <p class="card-text">${hit.recipe.source}</p>
-                    <a href="${hit.recipe.url}" class="btn btn-primary" target="_blank">Let's go!</a>
+                    <a href="${hit.recipe.url}" class="btn btn-default" target="_blank">Let's go!</a>
                 </div>
         </div>
     </div>
     `
     });
-    
+
     document.querySelector("#recipe-results").innerHTML = resultsHTML;
 }
+
+// Initialize AOS (www.https://michalsnik.github.io/aos/)
+function aos_init() {
+    AOS.init({
+        duration: 400,
+        easing: "ease-in-out",
+        once: false,
+        mirror: false
+    });
+}
+$(window).on('load', function () {
+    aos_init();
+});
