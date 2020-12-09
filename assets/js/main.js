850964652de589972237cde4b6f4c48553339ec1
Deny Tsjapanov <d.tsjapanov@gmail.com>
2020-12-09 02:02:14 +0100

    Added Math.floor to Kcal count and more styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     });
 });
 
-// Event listener for button press on "Enter" press
+// Event listener for button press on "Enter" press (https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp)
 var input = document.getElementById("search");
 input.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
@@ -62,7 +62,8 @@ function updateHTML(data) {
             <img src="${hit.recipe.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${hit.recipe.label}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${hit.recipe.healthLabels[length]}<br>${hit.recipe.calories} kCal.</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">${hit.recipe.healthLabels[0]}, ${hit.recipe.healthLabels[1]}, ${hit.recipe.healthLabels[2]}, ${hit.recipe.healthLabels[3]}, ${hit.recipe.healthLabels[4]}, ${hit.recipe.healthLabels[5]} 
+                    <br><span id="calories" class="card-subtitle">${Math.floor(hit.recipe.calories)} kCal</span></h6>
                     <p class="card-text">${hit.recipe.source}</p>
                     <a href="${hit.recipe.url}" class="btn btn-default" target="_blank">Let's go!</a>
                 </div>
